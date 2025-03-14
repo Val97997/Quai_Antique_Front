4c5e027ee2a9b91f3e75566c716d3287ed6b48e2
thiérion guilhem <107396565+Val97997@users.noreply.github.com>
2025-03-14 13:31:41 +0100

    first commit

---
+++ Router/allRoutes.js
@@ -0,0 +1,10 @@
+import Route from "./Route.js";
+
+// Définir ici vos routes avec l'ajout des nouvelles pages :
+export const allRoutes = [
+    new Route("/", "Accueil", "/pages/home.html"),
+    // new Route("Galerie", "Galerie", "/pages/galerie.html", "/js/galerie.js"),
+];
+
+// le titre s'affiche comme ceci : Route.titre - websitename
+export const websitename = "Quai Antique";
